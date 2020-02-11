f2fff09a9e5fe1eec7c69798c5685f8697827eba
abner <abner@qq.com>
2020-02-11 13:33:52 +0800

    fix

--- udp.php
+++ udp.php
@@ -1,10 +1,11 @@
 <?php
-
 $host = '0.0.0.0';
 $port = 9502;
 $mode = SWOOLE_PROCESS;  //多进程模式
 $sock_type = SWOOLE_SOCK_UDP; //udp服务
+echo "before init \n";
 $serv = new swoole_server($host, $port, $mode, $sock_type);
+echo "init over \n";
 
 //监听数据接受的事件
 /*
@@ -13,6 +14,7 @@ $serv = new swoole_server($host, $port, $mode, $sock_type);
  * $fd 客户端信息
  */
 $serv->on('packet',function($serv,$data,$fd){
+    echo "begin receive\n";
     //发送数据到相应客户端,反馈信息
     $ip = $fd['address'];
         $port = $fd['port'];
