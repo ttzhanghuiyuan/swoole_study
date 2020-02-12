42751e6c0632a1365e4149edc6c6456bfe176f08
abner <abner@qq.com>
2020-02-12 14:02:52 +0800

    fix

--- asyncReadFile.php
+++ asyncReadFile.php
@@ -1,6 +1,6 @@
 <?php
 //异步文件读取
 
-swoole_async_read(__DIR__.'/1.txt', function($filename,$content){
+swoole_async_readfile(__DIR__.'/1.txt', function($filename,$content){
     echo "{$filename} {$content}";
 });
