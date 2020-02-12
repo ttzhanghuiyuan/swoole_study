78cd3d64e94521b073b11c814531ef0654b9737d
abner <abner@qq.com>
2020-02-12 12:08:37 +0800

    lock

---
+++ lock.php
@@ -0,0 +1,27 @@
+<?php
+
+//锁机制
+
+
+//创建锁对象
+$type = SWOOLE_MUTEX;  //互斥锁
+$lock = new swoole_lock($type);
+echo "创建互斥锁 \n";
+
+$lock->lock();  //开始锁定主进程
+
+if(pcntl_fork() > 0){
+    sleep(1);
+    $lock->unlock(); //解锁
+}else{
+    echo "子进程等待锁 \n";
+    $lock->lock();//上锁
+    echo "子进程获取锁";
+    $lock->unlock(); //释放锁
+    exit("子进程退出");
+}
+
+echo "主进程 释放锁";
+unset($lock);
+sleep(1);
+echo "子进程退出";
