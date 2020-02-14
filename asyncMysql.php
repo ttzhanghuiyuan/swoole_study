e207da00903b813e5ac7246ca5b6087a96f982e3
abner <abner@qq.com>
2020-02-14 12:21:29 +0800

    asyncMysql

---
+++ asyncMysql.php
@@ -0,0 +1,38 @@
+<?php
+
+//异步mysql操作
+
+
+//实例化资源
+$db = new swoole_mysql();
+
+$config = [
+    'host' => '',
+    'user' => '',
+    'password' => '',
+    'database' => '',
+    'charset' => 'utf8',
+];
+
+//连接数据
+$db->connect($config,function($db,$r){
+    if($r == false){
+        var_dump($db->connect_errno, $db->connect_error);
+        die("失败");
+    }
+
+    //成功的逻辑
+    $sql = ' show tables';
+    $db->query($sql, function(swoole_mysql $db, $r){
+        if($r == false){
+            var_dump($db->error);
+            die("操作失败");
+        }elseif($r == false){
+            var_dump($db->affected_rows, $db->insert_id);
+        }
+
+        var_dump($r);
+        $db->close();
+    });
+
+});
